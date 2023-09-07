72ec8c14b09ba9923200c842fb0ad00f31f6a89d
Baruch Giverts <bor770@gmail.com>
2023-09-07 17:30:34 +0300

    in middle

--- src/app/main/toggle/toggle.component.ts
+++ src/app/main/toggle/toggle.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LetDirective } from '@ngrx/component';
+import { Observable } from 'rxjs';
+
+import { BaseComponent } from '../../shared/base/base.component';
+import * as fromRoot from '../../store/root.reducer';
 
 @Component({
+  imports: [CommonModule, LetDirective],
   selector: 'app-toggle',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./toggle.component.css'],
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.css']
 })
-export class ToggleComponent {
+export class ToggleComponent extends BaseComponent implements OnInit {
+  expanded$: Observable<boolean>;
 
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    this.expanded$ = this.store.select(fromRoot.selectUIExpanded);
+  }
 }
